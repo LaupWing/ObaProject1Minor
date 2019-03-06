e9bb205dd28fff0a0164b5c44cd356e0f94cd6b9
LaupWing <loc_strict@hotmail.com>
2019-03-06 21:06:11 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -22,21 +22,52 @@ import {API} from "../../modules/OBA-wrapper-master/js/index.js"
 //         console.log(response)
 //     }
 // })()
-// async function init() {
-//     const api = new API({
-//       key: "1e19898c87464e239192c8bfe422f280"
-//     })
-//     const stream = await api.createStream("search/banaan")
-//     stream
-//       .pipe(toJSON)
-// }
+async function init() {
+    const api = new API({
+      key: "1e19898c87464e239192c8bfe422f280"
+    })
+    const stream = await api.createStream("search/banaan{1000}")
+    return  stream
+                .pipe(toJSON)
+                    .all()
+                        .then(allItems)
+}
   
-// function toJSON(stream) {
-// console.log(stream);
-// }
-// init()
+function allItems(items){
+    const array = items.flat()
+    return array
+}
 
-function getBg(subject = "library"){
+function toJSON(stream) {
+    return stream
+}
+
+function jwzz(){
+    // console.log(init())
+    const test = init()
+    console.log()
+    test.then(x=>{
+        console.log(x)
+        const cleanData = x.filter(cutUndefined)
+        // console.log(cleanData)/
+        const filteredArray = cleanData.filter(filterArray)
+        console.log(filteredArray)
+    })
+}
+
+function cutUndefined(item){
+    return item !== undefined
+}
+
+function filterArray(item){
+    return item.genres[0] !== undefined 
+}
+
+// jwzz()
+// console.log(init())
+
+function getBg(subject, storage){
+    console.log(subject, storage)
     fetch(`https://source.unsplash.com/1600x900?${subject}`)
         .then(response=>{
             const bgImage = {
@@ -44,32 +75,35 @@ function getBg(subject = "library"){
                 date: new Date().getDate()
             }
             console.log(bgImage)
-            localStorage.setItem("Background", JSON.stringify(bgImage))
+            localStorage.setItem(storage, JSON.stringify(bgImage))
             setBg(bgImage.url)
         })
 }
 
-function checkLocalStorage(item, callback){
+function checkLocalStorage(item, callback, bgSetting){
     const storage = JSON.parse(localStorage.getItem(item))
     if(!storage){
         console.log("LocalStorage niet aanwezig")
-        getBg()
+        getBg(bgSetting.searchterm, bgSetting.storage)
         return
     }
     console.log("LocalStorage aanwezig")
     callback(storage)
 }
 
-function checkDay(obj){
+function checkDay(obj, bgSetting){
+    console.log()
     const day = new Date().getDate()
     if(obj.date === day){
         console.log("Tzelfde dag, geen nieuwe bgImg")
         setBg(obj.url)
     }else{
         console.log("Niet tzelfde dag, nieuwe bgImg")
-        getBg()
+        getBg(bgSetting.searchterm, bgSetting.storage)
     }
 }
+
+document.querySelector("a")
 
 function setBg(url){
     document.body.style.background = `linear-gradient(
@@ -83,11 +117,16 @@ genreContainer.forEach(item=>addEvents(item))
 function addEvents(item){
     item.addEventListener("mouseover", classToggle)
     item.addEventListener("mouseout", classToggle)
+    item.addEventListener("click", handleclick)
 }
 
+function handleclick(){
+    const genre = this.querySelector("svg").classList.value
+    console.log(genre)
+    checkLocalStorage(`${genre}BG`, checkDay, {storage:`${genre}BG`, searchterm:`${genre}`})
+}
 
 function classToggle(){
-    console.log(this.querySelector("svg").id)
     this.classList.toggle(this.querySelector("svg").id)
     this.querySelectorAll("svg path").forEach(path=>{
         path.classList.toggle("anim")
@@ -97,21 +136,72 @@ function classToggle(){
     }
 }
 
+window.addEventListener("hashchange", renderGenre)
 
-checkLocalStorage("Background", checkDay)
+function renderGenre(){
+    clearContainer()
+    const genre = window.location.hash.slice(1)
+    const firstLetter = genre.slice(0,1)
+    const restGenre = genre.slice(1)
+    const genresContainer = document.querySelector(".genres")
+    genresContainer.classList.add("genrePage")
+    document.querySelector(".container").classList.add("genrePage")
+    document.querySelector("h1").innerHTML = `
+        <span>${firstLetter}</span>${restGenre}
+    `
+    const newElement = 
+    `
+        <select name="" id="datum">
+            <option value="">Deze Week</option>
+            <option value="">Deze Maand</option>
+            <option value="">Dit Jaar</option>
+        </select>
+        <div class="tops-wrapper">
+        <i class="fas fa-angle-left"></i>
+            <div class="tops">
+            </div>
+        <i class="fas fa-angle-right"></i>
+        </div>
+    `
+    genresContainer.insertAdjacentHTML( 'beforeend', newElement )
+    addImagesTops()
+}
+
+function addImagesTops(){
+    init()
+    .then(x=>{
+            const tops = document.querySelector(".tops")
+            console.log("addingImages")
+            const cleanData = x.filter(cutUndefined)
+            console.log(cleanData)
+            for(let i=20; i<30; i++){
+                console.log("itereren")
+                const newElement =
+                `
+                    <div class="img-wrapper"> 
+                        <img src="${cleanData[i].images[0]}"> </img>
+                    </div>
+                `
+                console.log(newElement)
+                tops.insertAdjacentHTML("beforeend", newElement)
+            }
+            highlight()
+        })
+}
+
+function highlight(){
+    const imgwrapper = document.querySelectorAll(".img-wrapper")
+    imgwrapper[0].classList.add("highlighted")
+}
+
+function clearContainer(){
+    const genres = document.querySelector(".genres")
+    console.log(genres)
+    while(genres.firstChild){
+        genres.removeChild(genres.firstChild)
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+checkLocalStorage("LandingBG", checkDay, {storage: "LandingBG", searchterm:"library"})
