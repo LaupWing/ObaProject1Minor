adc8b21fa7b76a1d809234ddc0b719966e99e8c5
LaupWing <loc_strict@hotmail.com>
2019-03-14 10:43:30 +0100

    f

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const port = 3000
+// import {getITems} from "./"
 
 app.listen(port)
 app.set('view engine', 'ejs')
 app.set("views", "view")
 app.get("/", renderHome)
+
 app.use(express.static("static"))
 function renderHome(req, res){
     res.render('index')
